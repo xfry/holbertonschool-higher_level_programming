39f0bef1c653b1dcfb2f182efe9bfeedfb72fa93
xfry <fry.andrade@gmail.com>
2019-04-26 02:14:50 +0000

    delete print lines

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.find("with"))
-print(str.find("very"))
 str = str[39:67] + str[107:112] + str[0:6]
 print(str)
