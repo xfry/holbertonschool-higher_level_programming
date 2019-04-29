3fd0df5fb58ca89680aac70218573f3429f65765
xfry <fry.andrade@gmail.com>
2019-04-29 22:48:46 +0000

    solve another typo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,12 +4,12 @@ number = random.randint(-10000, 10000)
 lastDigit = number % 10
 if number < 0:
     lastDigit = (-1 * number) % 10
-msg1 = "last digit of {:d} is {:d} and is greater than 5"
-msg3 = "last digit of {:d} is {:d} and is less than 6 and not 0"
+msg1 = "Last digit of {:d} is {:d} and is greater than 5"
+msg3 = "Last digit of {:d} is {:d} and is less than 6 and not 0"
 
 if lastDigit > 5:
     print(msg1.format(number, lastDigit))
 elif lastDigit == 0:
-    print("last digit of {:d} is {:d} and is 0".format(number, lastDigit))
+    print("Last digit of {:d} is {:d} and is 0".format(number, lastDigit))
 elif lastDigit < 6 & number != 0:
     print(msg3.format(number, lastDigit))
