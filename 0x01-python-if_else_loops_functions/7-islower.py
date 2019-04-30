1b6e186b8ae4fe96079c32ff34fd8b3e841e3050
xfry <fry.andrade@gmail.com>
2019-04-30 20:42:59 +0000

    fix logic

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     for n in range(97, 123):
-        if ord(c) == 99:
+        if n == ord(c):
             return True
-        else:
-            return False
+    return False
