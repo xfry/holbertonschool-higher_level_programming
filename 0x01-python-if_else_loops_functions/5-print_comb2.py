c43c935794f8f40a79307f6761afac8be3ea74db
xfry <fry.andrade@gmail.com>
2019-04-30 16:14:10 +0000

    remove space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for n in range(0, 100):
     if n < 10:
         print("{:02.0f}{:s} ".format(n, ','), end='')
     else:
-        print("{:d}{:s} ".format(n, (',' if n != 99 else ' ') ), end='')
+        print("{:d}{:s} ".format(n, (',' if n != 99 else '')), end='')
