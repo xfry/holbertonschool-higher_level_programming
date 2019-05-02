46a8de8f5eb47cb9727e13d6924ab871e7393b74
xfry <fry.andrade@gmail.com>
2019-05-02 21:01:44 +0000

    fix the algorithms

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDigit = number % 10
 if number < 0:
     lastDigit = ((-1 * number) % 10) * -1
+else:
+    lastDigit = number % 10
 msg1 = "Last digit of {:d} is {:d} and is greater than 5"
 msg3 = "Last digit of {:d} is {:d} and is less than 6 and not 0"
 
