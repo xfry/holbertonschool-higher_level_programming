3eb460f07e348823350c8c2035d5021728d62499
xfry <fry.andrade@gmail.com>
2019-05-02 19:18:27 +0000

    removed multiple spaces before operators

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     up_letters = range(65, 91)
-    low_letters  = range(97, 127)
+    low_letters = range(97, 127)
     new_str = ""
     for character in str:
         if ord(character) > 64 and ord(character) < 91:
